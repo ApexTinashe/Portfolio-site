3dc1433dec2ac7d65b8bd8425f1bc1e5e996182e
ApexTinashe <111948041+ApexTinashe@users.noreply.github.com>
2023-07-09 22:15:14 +0200

    Add files via upload

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,11 @@
+import typed from "typed-js"
+
+var typed = new Typed('.multiple-text', 
+{
+	strings:['Frontend Developer', 'Webscraper', 
+		'SEO Marketer'],
+	typeSpeed:100,
+	backSpeed:100,
+	backDelay: 1000,
+	loop: true
+})
